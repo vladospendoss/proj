cd12637b74061a987324ef3ebb290bfb2cd14a42
Vladislav Savenko <vladislav.savenko1996@gmail.com>
2025-06-07 13:42:07 +0300

    улучшили логику main и doSomething

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,11 +4,12 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("101");
+        System.out.println("---");
     }
 
     public static void doSomething() {
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(0);
+        System.out.println(0);
     }
 }
