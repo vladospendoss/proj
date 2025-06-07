2ea0ad73b7c540d1d5b906280159ae334f6a6e01
Vladislav Savenko <vladislav.savenko1996@gmail.com>
2025-06-07 13:35:48 +0300

    Изменена логика Добавлен класс Box

---
+++ src/main/java/com/stepup/proj/Box.java
@@ -0,0 +1,20 @@
+package com.stepup.proj;
+
+public class Box {
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Box() {
+    }
+
+    public Box(int size){
+        this.size = size;
+    }
+}
--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,6 +3,10 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("111");
+        System.out.println("101");
+    }
+
+    public static void doSomething() {
+        System.out.println(1);
     }
 }
