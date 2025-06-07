77897d05a00dbf6a92275d4f79efade61022647a
Vladislav Savenko <vladislav.savenko1996@gmail.com>
2025-06-07 13:27:40 +0300

    Доработка логики проекта

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,5 +3,6 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("111");
     }
 }
